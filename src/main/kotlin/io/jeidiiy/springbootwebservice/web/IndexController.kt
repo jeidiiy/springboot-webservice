2b7d0db7c68c06054d9b04cee476a6e1d3def85d
Jeidiiy <jeidiiy98@gmail.com>
2023-03-19 23:17:07 +0900

    feat: 게시글 수정 기능 개발

--- src/main/kotlin/io/jeidiiy/springbootwebservice/web/IndexController.kt
+++ src/main/kotlin/io/jeidiiy/springbootwebservice/web/IndexController.kt
@@ -1,15 +1,24 @@
 package io.jeidiiy.springbootwebservice.web
 
 import io.jeidiiy.springbootwebservice.service.posts.PostsService
+import io.jeidiiy.springbootwebservice.web.dto.PostsResponseDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class IndexController(
     @Autowired val postsService: PostsService
 ) {
+    @GetMapping("/posts/update/{id}")
+    fun postsUpdate(@PathVariable id: Long, model: Model): String {
+        val dto: PostsResponseDto = postsService.findById(id)
+        model.addAttribute("post", dto)
+        return "posts-update"
+    }
+
     @GetMapping("/")
     fun index(model: Model): String {
         model.addAttribute("posts", postsService.findAllDesc())
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -2,6 +2,10 @@ const init = () => {
     $('#btn-save').on('click', () => {
         save()
     });
+
+    $('#btn-update').on('click', () => {
+        update();
+    })
 };
 
 const save = () => {
@@ -22,6 +26,28 @@ const save = () => {
     }).done(() => {
         alert('글이 등록되었습니다.');
         window.location.href = '/'
+    }).fail((error) => {
+        alert(JSON.stringify(error));
+    });
+}
+
+const update = () => {
+    const data = {
+        title: $('#title').val(),
+        content: $('#content').val()
+    };
+
+    const id = $('#id').val();
+
+    $.ajax({
+        type: 'PUT',
+        url: `/api/v1/posts/${id}`,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data)
+    }).done(() => {
+        alert('글이 수정되었습니다.');
+        window.location.href = '/';
     }).fail((error) => {
         alert(JSON.stringify(error));
     });
