5984298c542fe94ce3d4999a7bc5d7c59ca243e3
Jeidiiy <jeidiiy98@gmail.com>
2023-03-19 18:42:45 +0900

    feat: HelloDto API 개발 및 테스트

--- src/main/kotlin/io/jeidiiy/springbootwebservice/web/HelloController.kt
+++ src/main/kotlin/io/jeidiiy/springbootwebservice/web/HelloController.kt
@@ -1,6 +1,8 @@
 package io.jeidiiy.springbootwebservice.web
 
+import io.jeidiiy.springbootwebservice.web.dto.HelloResponseDto
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -8,4 +10,10 @@ class HelloController {
 
     @GetMapping("/hello")
     fun hello(): String = "hello"
+
+    @GetMapping("/hello/dto")
+    fun helloDto(
+        @RequestParam name: String,
+        @RequestParam amount: Int
+    ) = HelloResponseDto(name, amount)
 }
--- src/test/kotlin/io/jeidiiy/springbootwebservice/web/HelloControllerTest.kt
+++ src/test/kotlin/io/jeidiiy/springbootwebservice/web/HelloControllerTest.kt
@@ -1,12 +1,12 @@
 package io.jeidiiy.springbootwebservice.web
 
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @WebMvcTest(controllers = [HelloController::class])
 class HelloControllerTest(
@@ -19,5 +19,20 @@ class HelloControllerTest(
         mvc.perform(get("/hello"))
             .andExpect(status().isOk)
             .andExpect(content().string(hello))
+    }
+
+    @Test
+    fun helloDto가_리턴된다() {
+        val name = "hello"
+        val amount = 1000
+
+        mvc.perform(
+            get("/hello/dto")
+                .param("name", name)
+                .param("amount", amount.toString())
+        )
+            .andExpect(status().isOk)
+            .andExpect(jsonPath("$.name", `is`(name)))
+            .andExpect(jsonPath("$.amount", `is`(amount)))
     }
 }
