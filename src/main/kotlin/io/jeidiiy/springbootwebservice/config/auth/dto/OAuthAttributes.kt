452226ee0c90455110d821af0043851d9a9f5ae2
Jeidiiy <jeidiiy98@gmail.com>
2023-03-20 11:57:57 +0900

    feat: 네이버 소셜 로그인 기능 추가

--- src/main/kotlin/io/jeidiiy/springbootwebservice/config/auth/dto/OAuthAttributes.kt
+++ src/main/kotlin/io/jeidiiy/springbootwebservice/config/auth/dto/OAuthAttributes.kt
@@ -12,7 +12,23 @@ class OAuthAttributes(
 ) {
     companion object {
         fun of(registrationId: String, userNameAttributeName: String, attributes: Map<String, Any>) =
-            ofGoogle(userNameAttributeName, attributes)
+            when (registrationId) {
+                "naver" -> ofNaver("id", attributes)
+                "google" -> ofGoogle(userNameAttributeName, attributes)
+                else -> throw IllegalArgumentException("No registration Id$: {registrationId}")
+            }
+
+        private fun ofNaver(userNameAttributeName: String, attributes: Map<String, Any>): OAuthAttributes {
+            val response = attributes["response"] as Map<String, Any>
+
+            return OAuthAttributes(
+                name = response["name"] as String,
+                email = response["email"] as String,
+                picture = response["profile_image"] as String,
+                attributes = response,
+                nameAttributeKey = userNameAttributeName
+            )
+        }
 
         private fun ofGoogle(userNameAttributeName: String, attributes: Map<String, Any>) =
             OAuthAttributes(
